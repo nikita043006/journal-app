31d81be3bd1064f0e51f47060d475680415d35a9
Nikita <nikita043006@users.noreply.github.com>
2026-01-11 19:51:33 +0530

    initial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,10 @@
+package net.engineeringdigest.journalApp.repository;
+
+
+import net.engineeringdigest.journalapp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
